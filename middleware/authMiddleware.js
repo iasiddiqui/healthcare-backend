d6217064dbff51bc63d888c41b1a91fc054e8e7a
iasiddiqui <115731094+iasiddiqui@users.noreply.github.com>
2025-06-19 19:23:58 +0530

    chnage in middleware

--- middleware/authMiddleware.js
+++ middleware/authMiddleware.js
@@ -1,15 +1,17 @@
-const jwt = require('jsonwebtoken');
-require('dotenv').config();
+const jwt = require("jsonwebtoken");
+require("dotenv").config();
 
 module.exports = (req, res, next) => {
-  const token = req.header('Authorization');
-  if (!token) return res.status(401).json({ msg: 'No token' });
+  const token = req.header("Authorization");
+  if (!token) return res.status(401).json({ msg: "No token" });
 
   try {
     const decoded = jwt.verify(token, process.env.JWT_SECRET);
     req.user = decoded.user;
     next();
   } catch (err) {
-    res.status(401).json({ msg: 'Token invalid' });
+    console.log("JWT ERROR:", err.message); // ✅ ADD THIS
+    res.status(401).json({ msg: "Token invalid" });
   }
 };
+
